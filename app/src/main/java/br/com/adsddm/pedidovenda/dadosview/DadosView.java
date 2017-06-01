3f58c085ae7cfeb800043f60f084e5e442bafb68
danielOliSouza <daniel.backspace@gmail.com>
2017-06-01 15:26:55 -0300

    Dados usados na visão em um classe singleton

---
+++ app/src/main/java/br/com/adsddm/pedidovenda/dadosview/DadosView.java
@@ -0,0 +1,58 @@
+package br.com.adsddm.pedidovenda.dadosview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.adsddm.pedidovenda.model.Cliente;
+import br.com.adsddm.pedidovenda.model.PedidoVenda;
+import br.com.adsddm.pedidovenda.model.Produto;
+
+/**
+ * Created by Daniel on 29/05/2017.
+ * Dados que deverão ser usados pelas as activity
+ */
+
+public class DadosView {
+    private static DadosView _instance = null;
+
+    private PedidoVenda pedidoVenda;
+    private List<Produto> produtos;
+    private List<Cliente> clientes;
+
+    private DadosView(){
+        pedidoVenda = new PedidoVenda();
+        produtos = new ArrayList<>();
+        clientes = new ArrayList<>();
+    }
+
+    public static DadosView Instance(){ //Singleton
+        if(_instance == null){
+            _instance = new DadosView();
+        }
+        return _instance;
+    }
+
+    public PedidoVenda getPedidoVenda() {
+        return pedidoVenda;
+    }
+
+    public void setPedidoVenda(PedidoVenda pedidoVenda) {
+        this.pedidoVenda = pedidoVenda;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+}
